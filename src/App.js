4f49f6a5e07fae9f384624a32fb583e00b724a1f
Salvador Dieffenbach <zogwarg@hotmail.com>
2019-04-14 00:12:07 -0400

    Initial Setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <header className="jumbotron">
+          <h1 className="display-1">Google Book Search</h1>
+          <h4 className="text-muted">Search for and Save Books of Interst</h4>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
